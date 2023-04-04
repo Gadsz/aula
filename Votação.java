3a53d2b9be7276e54dc4714023f0e42d94b3865d
Gabriel Albuquerque da Silva <82517993+Gadsz@users.noreply.github.com>
2023-04-04 20:09:10 -0300

    Add files via upload
    Update de código: 4 de Abril, 2023 - 20:09

--- Votação.java
+++ Votação.java
@@ -3,19 +3,27 @@ import java.util.Scanner;
 class Votação{
     public static void main(String[] args){
         //Variáves
-        int valor, votos;
+        int brancos, nulos, votos, c1, c2, c3; 
         Scanner entrada;
 
         //Entrada
         entrada = new Scanner(System.in);
-        System.out.print("Digite um valor: ");
-        votos = entrada.nextInt();
+        System.out.print("Quantos votos teve o canditado 1: ");
+        c1 = entrada.nextInt();
+        System.out.print("Quantos votos teve o canditado 2: ");
+        c2 = entrada.nextInt();
+        System.out.print("Quantos votos teve o canditado 3: ");
+        c3 = entrada.nextInt();
+        System.out.print("Quantos votos em brancos ou nulos: ");
+        brancos = entrada.nextInt();
         entrada.close();
 
         //Processamento
-        cubo = valor * valor * valor;
+        votos = 
 
         //Saída
-        System.out.printf("Valor: %d.\n", cubo); 
+        System.out.printf("Total: ", votos);
+        System.out.printf("Total: ", votos);
+        System.out.printf("Total: ", votos);
     }
 }
