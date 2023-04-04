405fcb09c5693d4354b92b05a22cb6ea69c808d5
Gabriel Albuquerque da Silva <82517993+Gadsz@users.noreply.github.com>
2023-04-04 20:23:41 -0300

    Add files via upload
    Update código: 4 de Abril, 2023 - 20:23
    Log: Votação funciona

--- Votação.java
+++ Votação.java
@@ -3,27 +3,30 @@ import java.util.Scanner;
 class Votação{
     public static void main(String[] args){
         //Variáves
-        int brancos, nulos, votos, c1, c2, c3; 
+        int votos;
+        float total, brancos, c1, c2, c3, porc1, porc2, porc3, porcbranco;
         Scanner entrada;
 
         //Entrada
         entrada = new Scanner(System.in);
         System.out.print("Quantos votos teve o canditado 1: ");
-        c1 = entrada.nextInt();
+        c1 = entrada.nextFloat();
         System.out.print("Quantos votos teve o canditado 2: ");
-        c2 = entrada.nextInt();
+        c2 = entrada.nextFloat();
         System.out.print("Quantos votos teve o canditado 3: ");
-        c3 = entrada.nextInt();
+        c3 = entrada.nextFloat();
         System.out.print("Quantos votos em brancos ou nulos: ");
-        brancos = entrada.nextInt();
+        brancos = entrada.nextFloat();
         entrada.close();
 
         //Processamento
-        votos = 
+        total = c1 + c2 + c3 + brancos;
+        porc1 = c1 * 100 / total;
+        porc2 = c2 * 100 / total;
+        porc3 = c3 * 100 / total;
+        porcbranco = brancos * 100 / total;
 
         //Saída
-        System.out.printf("Total: ", votos);
-        System.out.printf("Total: ", votos);
-        System.out.printf("Total: ", votos);
+        System.out.printf("%.2f Votaram em C1.\n%.2f Votaram em C2.\n%.2f Votaram em C3.\n", porc1, porc2, porc3);
     }
 }
