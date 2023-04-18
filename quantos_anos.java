4f58c3f406a4beddba46d52e6d485c227cc2a7be
Gabriel Albuquerque da Silva <82517993+Gadsz@users.noreply.github.com>
2023-04-18 19:43:42 -0300

    Add files via upload

---
+++ quantos_anos.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//Aula sobre if e if else do dia 18 de Abril, 2023
+class quantos_anos {
+    public static void main(String[] args) {
+    int idade;
+    Scanner entrada;
+
+//entrada
+    entrada = new Scanner(System.in);
+    System.out.println("Digite sua idade");
+    idade = entrada.nextInt();
+    entrada.close();
+
+//processamento e saída
+    System.out.printf("Parabéns! Você tem %d anos \n", idade);
+    
+    if( idade>=18)
+    {
+        System.out.println("Você é maior de idade");
+    }
+
+    }
+}
